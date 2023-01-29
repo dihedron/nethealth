99e6f9e60e485ba90bef92f6bf58e666688b4c50
Andrea Funtò <dihedron.dev@gmail.com>
2023-01-29 16:34:45 +0100

    Add ping

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ binary:
 #	@goreleaser build --single-target --snapshot --rm-dist
 	@goreleaser build --snapshot --rm-dist
 	@cp dist/nethealth_linux_amd64_v3/nethealth .
-
+	@sudo setcap cap_net_raw=+ep ./nethealth
 
 .PHONY: clean
 clean:
--- cmd/nethealth/command/start/command.go
+++ cmd/nethealth/command/start/command.go
@@ -1,9 +1,12 @@
 package start
 
 import (
+	"fmt"
+	"sync"
 	"time"
 
 	"github.com/dihedron/nethealth/cmd/nethealth/command/base"
+	probing "github.com/prometheus-community/pro-bing"
 )
 
 type Duration time.Duration
@@ -16,12 +19,43 @@ type Topology struct {
 type Start struct {
 	base.Command
 
-	Topology Topology `short:"t" long:"topology" description:"The network endpoints to ping." required:"yes"`
+	Topology Topology `short:"t" long:"topology" description:"The network endpoints to ping." optional:"yes"`
 }
 
 func (cmd *Start) Execute(args []string) (err error) {
 	logger := cmd.InitLogger(true)
 
 	logger.Debug("starting...")
+
+	var wg sync.WaitGroup
+
+	for _, address := range []string{
+		"www.google.com",
+		// "www.repubblica.it",
+		"www.bbc.co.uk",
+	} {
+
+		wg.Add(1)
+
+		go func(address string) {
+			pinger, err := probing.NewPinger(address)
+			if err != nil {
+				panic(err)
+			}
+			pinger.Count = 10
+			pinger.SetPrivileged(true)
+			pinger.Interval = 1 * time.Millisecond
+			err = pinger.Run() // Blocks until finished.
+			if err != nil {
+				panic(err)
+			}
+			stats := pinger.Statistics() // get send/receive/duplicate/rtt stats
+			fmt.Printf("stats to %s: %v\n", address, stats)
+			wg.Done()
+		}(address)
+	}
+
+	wg.Wait()
+	fmt.Printf("done!\n")
 	return nil
 }
